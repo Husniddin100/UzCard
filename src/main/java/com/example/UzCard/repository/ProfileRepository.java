bbb16d338f47dd2380c90ab94fb2416eae2f0ec1
husniddin100 <uchanal10@gmail.com>
2024-03-04 18:33:38 +0500

    change status method

--- src/main/java/com/example/UzCard/repository/ProfileRepository.java
+++ src/main/java/com/example/UzCard/repository/ProfileRepository.java
@@ -1,6 +1,9 @@
 package com.example.UzCard.repository;
 
 import com.example.UzCard.entity.ProfileEntity;
+import com.example.UzCard.enums.ProfileStatus;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -11,4 +14,9 @@ public interface ProfileRepository extends CrudRepository<ProfileEntity, String>
     Optional<ProfileEntity> findByUsername(String username);
 
     Optional<ProfileEntity> findByUsernameAndPassword(String username, String password);
+
+    @Modifying
+    @Transactional
+    @Query("update ProfileEntity set status=?2  where id=?1")
+    void changeProfileStatus(String id, ProfileStatus status);
 }
