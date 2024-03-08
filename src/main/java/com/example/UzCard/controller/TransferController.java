7d9621ec9bea926e564a681e3c89c826295fcc85
Husniddin <uchanal10@gmail.com>
2024-03-08 18:31:18 +0500

    transfer +transaction controller service repository

---
+++ src/main/java/com/example/UzCard/controller/TransferController.java
@@ -0,0 +1,13 @@
+package com.example.UzCard.controller;
+
+import com.example.UzCard.service.TransferService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transfer")
+public class TransferController {
+    @Autowired
+    private TransferService transferSerivce;
+}
